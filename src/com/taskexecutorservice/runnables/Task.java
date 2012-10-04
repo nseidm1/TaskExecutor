28af87434bbaec1b3e5a37a9ca007b90c3cb1770
nseidman <nseidman@nseidman-w1.roundarch.com>
2012-10-04 16:55:24 -0400

    New stuff.

--- src/com/taskexecutorservice/runnables/Task.java
+++ src/com/taskexecutorservice/runnables/Task.java
@@ -2,34 +2,65 @@ package com.taskexecutorservice.runnables;
 
 import java.util.concurrent.Semaphore;
 
+import android.os.Handler;
+
 import com.taskexecutorservice.TaskExecutorService;
 import com.taskexecutorservice.callbacks.CompleteCallback;
 
 public abstract class Task implements Runnable {
-	private TaskExecutorService mTaskService;
+	private TaskExecutorService mTaskExecutorService;
 	private CompleteCallback mCompleteCallback;
 	private Semaphore mPause = new Semaphore(1);
 	private boolean mRemoveOnFail = false;
-
+	private Handler mUiHandler;
 	public abstract void defineTask() throws Exception;
 
-	public Task(TaskExecutorService taskService, CompleteCallback completeCallback) {
-		mTaskService = taskService;
+	/**
+	 * @param taskService
+	 * Provide a reference to the TaskExecutorService so when this task is complete it can remove itself from an existing queue.
+	 * @param completeCallback
+	 * Provide an interface callback to reporting when this task is complete.
+	 * @param uiHandler
+	 */
+	public Task(TaskExecutorService taskExecutorService, CompleteCallback completeCallback, Handler uiHandler) {
+		mTaskExecutorService = taskExecutorService;
 		mCompleteCallback = completeCallback;
+		if (uiHandler == null)
+		{
+			mUiHandler = new Handler();
+		}
+		else 
+		{
+			mUiHandler = uiHandler;
+		}
 	}
 
+	/**
+	 * @param completeCallback
+	 * Aside from the constructor you can specify the callback using this method.
+	 */
 	public void setCompleteCallback(CompleteCallback completeCallback) {
 		mCompleteCallback = completeCallback;
 	}
 
+	/**
+	 * @param removeOnFail
+	 * If the task fails to execute because of an exception, do you still want to remove it from the queue?
+	 */
 	public void setRemoveOnFail(Boolean removeOnFail) {
 		mRemoveOnFail = removeOnFail;
 	}
 
+	/**
+	 * 
+	 */
 	public void pause() {
 		mPause.tryAcquire();
 	}
 
+	/**
+	 * 
+	 */
 	public void resume() {
 		mPause.release();
 	}
@@ -39,13 +70,25 @@ public abstract class Task implements Runnable {
 		try {
 			mPause.acquire();
 			defineTask();
-			mTaskService.removeTaskFromQueue(this);
-			mCompleteCallback.onTaskCompletion(this, true, null);
-		} catch (Exception e) {
-			if (mRemoveOnFail)
-				mTaskService.removeTaskFromQueue(this);
-			if (mCompleteCallback != null)
-				mCompleteCallback.onTaskCompletion(this, false, e);
+			if (mTaskExecutorService != null)
+				mTaskExecutorService.removeTaskFromQueue(this);
+			mUiHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					if (mCompleteCallback != null)
+						mCompleteCallback.onTaskCompletion(Task.this, true, null);
+				}
+			});
+		} catch (final Exception e) {
+			if (mRemoveOnFail && mTaskExecutorService != null)
+				mTaskExecutorService.removeTaskFromQueue(this);
+			mUiHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					if (mCompleteCallback != null)
+						mCompleteCallback.onTaskCompletion(Task.this, false, e);
+				}
+			});
 		}
 	}
 }
