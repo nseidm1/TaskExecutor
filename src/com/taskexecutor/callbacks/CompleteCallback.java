9a3ee9aa68a5515a34cfa28428ca20dedcb47ed9
nseidm1 <noah@noahseidman.com>
2012-10-04 22:08:45 -0400

    Implemented an abstract FragmentActivity wrapping the TaskExecutor.

--- src/com/taskexecutor/callbacks/CompleteCallback.java
+++ src/com/taskexecutor/callbacks/CompleteCallback.java
@@ -4,5 +4,14 @@ import com.taskexecutor.runnables.Task;
 
 public interface CompleteCallback
 {
+	/**
+	 * @param task
+	 * The Task that was being executed.
+	 * @param success
+	 * If no exception was thrown this will be true and exception will be null, otherwise this will be false and exception will have information.
+	 * @param exception
+	 * Null if the task did not throw an exception.
+	 * 
+	 */
 	public void onTaskCompletion(Task task, boolean success, Exception exception);
 }
