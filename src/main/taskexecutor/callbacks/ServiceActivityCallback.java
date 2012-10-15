9f2a49eaa5e68bb4bd490cbfe227077d050ba0e4
nseidm1 <noah@noahseidman.com>
2012-10-14 22:30:23 -0400

    Javadoc improvement.

--- src/main/taskexecutor/callbacks/ServiceActivityCallback.java
+++ src/main/taskexecutor/callbacks/ServiceActivityCallback.java
@@ -2,7 +2,9 @@ package main.taskexecutor.callbacks;
 
 public interface ServiceActivityCallback{
     /**
-     * This only gets called if the Tasks are restored from disk and the Service is in CALLBACK_DEPENDENT mode.
+     * This only gets called if the Tasks are restored from disk and the Service is in CALLBACK_DEPENDENT mode. 
+     * If the Service is in CALLBACK_INCONSIDERATE 
+     * mode they will just be executed by the Service without a valid ui callback.
      */
     public void tasksHaveBeenRestored();
 }
